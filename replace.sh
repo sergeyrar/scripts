9e61d2becf24191418d25d2d655c6f870f4f0acc
sergey <sergeyrar@gmail.com>
2020-08-02 11:09:17 +0300

    Update replace.sh

--- replace.sh
+++ replace.sh
@@ -6,6 +6,11 @@
 if [ "$#" -ne 2 ]; then
 	echo "usage $0 source_string dest_string"
 else
-	echo "replacing $1 with $2 in workspace"
-	git grep -l "$1" | xargs sed -i "s/$1/$2/g"
+	git grep -n "$1"
+	read -p "Are you sure you want to replace $1 instances with $2 in workspace ?" -n 1 -r
+	echo
+	if [[ $REPLY =~ ^[Yy]$ ]]
+	then
+		git grep -l "$1" | xargs sed -i "s/$1/$2/g"
+	fi
 fi
